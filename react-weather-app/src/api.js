fc599a2fc146bb92ee429f267c6719cf59ed9927
Anthony Kabuthu <kabuthuantony@gmail.com>
2024-11-12 09:34:24 +0300

    Updates

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,11 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '65ca883c77mshd92e7f94105353fp1a3bc4jsncdd8f80c3284',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo"
+
